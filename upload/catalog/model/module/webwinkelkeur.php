a542194011ff5f39384032cbc8856cfc22eaf1da
Milko Kosturkov <milko@kiboit.com>
2017-11-28 10:31:23 +0100

    Use new API and send order data

--- upload/catalog/model/module/webwinkelkeur.php
+++ upload/catalog/model/module/webwinkelkeur.php
@@ -1,6 +1,60 @@
 <?php
 require_once DIR_SYSTEM . 'library/Peschar_URLRetriever.php';
 class ModelModuleWebwinkelkeur extends Model {
+    public function sendInvites() {
+        $settings = $this->getSettings();
+
+        if(empty($settings['shop_id']) ||
+           empty($settings['api_key']) ||
+           empty($settings['invite'])
+        )
+            return;
+
+        foreach($this->getOrdersToInvite($settings) as $order) {
+            $this->db->query("
+                UPDATE `" . DB_PREFIX . "order`
+                SET
+                    webwinkelkeur_invite_tries = webwinkelkeur_invite_tries + 1,
+                    webwinkelkeur_invite_time = " . time() . "
+                WHERE
+                    order_id = " . $order['order_id'] . "
+                    AND webwinkelkeur_invite_tries = " . $order['webwinkelkeur_invite_tries'] . "
+                    AND webwinkelkeur_invite_time = " . $order['webwinkelkeur_invite_time'] . "
+            ");
+            if($this->db->countAffected()) {
+                $parameters = array(
+                    'id'        => $settings['shop_id'],
+                    'code'  => $settings['api_key']
+                );
+                $post = array(
+                    'email'     => $order['email'],
+                    'order'     => $order['order_id'],
+                    'delay'     => $settings['invite_delay'],
+                    'language'      => str_replace('-', '_', $order['language_code']),
+                    'customer_name' => "$order[payment_firstname] $order[payment_lastname]",
+                    'phones' => [$order['telephone']],
+                    'client'    => 'opencart2',
+                    'platform_version' => VERSION,
+                    'plugin_version' => '1.1'
+                );
+                if($settings['invite'] == 2)
+                    $parameters['max_invitations_per_email'] = '1';
+
+                if (!$settings['limit_order_data']) {
+                    $post['order_data'] = json_encode($this->getOrderData($order));
+                }
+                $url = 'https://dashboard.webwinkelkeur.nl/api/1.0/invitations.json?' . http_build_query($parameters);
+                $retriever = new Peschar_URLRetriever();
+                $response = $retriever->retrieve($url, http_build_query($post));
+                if($this->isInviteSent($response)) {
+                    $this->db->query("UPDATE `" . DB_PREFIX . "order` SET webwinkelkeur_invite_sent = 1 WHERE order_id = " . $order['order_id']);
+                } else {
+                    $this->db->query("INSERT INTO `" . DB_PREFIX . "webwinkelkeur_invite_error` SET url = '" . $this->db->escape($url) . "', response = '" . $this->db->escape($response) . "', time = " . time());
+                }
+            }
+        }
+    }
+
     private function getOrdersToInvite($settings) {
         $max_time = time() - 1800;
 
@@ -33,50 +87,86 @@ class ModelModuleWebwinkelkeur extends Model {
         return $query->rows;
     }
 
-    public function sendInvites() {
-        $settings = $this->getSettings();
-
-        if(empty($settings['shop_id']) ||
-           empty($settings['api_key']) ||
-           empty($settings['invite'])
-        )
-            return;
-
-        foreach($this->getOrdersToInvite($settings) as $order) {
-            $this->db->query("
-                UPDATE `" . DB_PREFIX . "order`
-                SET
-                    webwinkelkeur_invite_tries = webwinkelkeur_invite_tries + 1,
-                    webwinkelkeur_invite_time = " . time() . "
-                WHERE
-                    order_id = " . $order['order_id'] . "
-                    AND webwinkelkeur_invite_tries = " . $order['webwinkelkeur_invite_tries'] . "
-                    AND webwinkelkeur_invite_time = " . $order['webwinkelkeur_invite_time'] . "
-            ");
-            if($this->db->countAffected()) {
-                $parameters = array(
-                    'id'        => $settings['shop_id'],
-                    'password'  => $settings['api_key'],
-                    'email'     => $order['email'],
-                    'order'     => $order['order_id'],
-                    'delay'     => $settings['invite_delay'],
-                    'lang'      => str_replace('-', '_', $order['language_code']),
-                    'customername' => "$order[payment_firstname] $order[payment_lastname]",
-                    'client'    => 'opencart',
-                );
-                if($settings['invite'] == 2)
-                    $parameters['noremail'] = '1';
-                $url = 'http://www.webwinkelkeur.nl/api.php?' . http_build_query($parameters);
-                $retriever = new Peschar_URLRetriever();
-                $response = $retriever->retrieve($url);
-                if(preg_match('|^Success:|', $response) || preg_match('|invite already sent|', $response)) {
-                    $this->db->query("UPDATE `" . DB_PREFIX . "order` SET webwinkelkeur_invite_sent = 1 WHERE order_id = " . $order['order_id']);
-                } else {
-                    $this->db->query("INSERT INTO `" . DB_PREFIX . "webwinkelkeur_invite_error` SET url = '" . $this->db->escape($url) . "', response = '" . $this->db->escape($response) . "', time = " . time());
-                }
-            }
-        }
+    private function isInviteSent($response) {
+        $result = @json_decode($response);
+        return is_object($result) && isset ($result->status)
+            && ($result->status == 'success' || strpos($result->message, 'already sent') !== false);
     }
+
+    private function getOrderData($order) {
+        $customer_fields = array(
+            'customer_id', 'customer_group_id',
+            'firstname', 'lastname',
+            'email', 'telephone', 'fax',
+            'custom_field'
+        );
+        $customer = array();
+        $invoice_address = array();
+        $delivery_address = array();
+        foreach ($order as $field => $value) {
+            if (in_array($field, $customer_fields)) {
+                $customer[$field] = $order[$field];
+            } else if (strpos($field, 'payment_') === 0) {
+                $new_field = str_replace('payment_', '', $field);
+                $invoice_address[$new_field] = $order[$field];
+            } else if (strpos($field, 'shipping_') === 0) {
+                $new_field = str_replace('shipping_', '', $field);
+                $delivery_address[$new_field] = $order[$field];
+            } else {
+                continue;
+            }
+            unset ($order[$field]);
+        }
+
+        $lines_query = "SELECT * FROM `" . DB_PREFIX . "order_product` WHERE `order_id` = {$order['order_id']}";
+        $order['order_lines'] = $this->db->query($lines_query)->rows;
+
+        $order_data = array(
+            'order' => $order,
+            'products' => $this->getOrderProducts($order),
+            'customer' => $customer,
+            'invoice_address' => $invoice_address,
+            'delivery_address' => $delivery_address
+        );
+
+        return $order_data;
+    }
+
+    private function getOrderProducts($order) {
+        $product_ids = array();
+        foreach ($order['order_lines'] as $line) {
+            if (!$line['product_id']) {
+                continue;
+            }
+            $product_ids[] = $line['product_id'];
+        }
+
+        if (empty ($product_ids)) {
+            return array();
+        }
+
+        $products_query = "
+          SELECT * 
+          FROM `" . DB_PREFIX . "product` AS `p`
+          LEFT JOIN `" . DB_PREFIX . "product_description` AS `pd`
+            ON `p`.`product_id` = `pd`.`product_id`
+                AND `pd`.`language_id` = {$order['language_id']}
+          WHERE `p`.`product_id` IN (" . join(',', $product_ids) . ')';
+        $products = $this->db->query($products_query)->rows;
+        $base_url = $this->request->server['REQUEST_SCHEME'] == 'https'
+            ? $this->config->get('config_ssl')
+            : $this->config->get('config_url');
+        foreach ($products as &$product) {
+            $images = array($base_url . 'image/' . $product['image']);
+            $image_query = "SELECT * FROM `" . DB_PREFIX . "product_image` WHERE `product_id` = {$product['product_id']}";
+            foreach ($this->db->query($image_query)->rows as $image) {
+                $images[] = $base_url . 'image/' . $image['image'];
+            }
+            $product['image_urls'] = $images;
+        }
+        return $products;
+    }
+
 
     public function getSettings() {
         $this->load->model('setting/setting');
--- upload/system/library/Peschar_URLRetriever.php
+++ upload/system/library/Peschar_URLRetriever.php
@@ -4,24 +4,32 @@
  */
 
 class Peschar_URLRetriever {
-    public function retrieve($url) {
-        if(($content = $this->retrieveWithCURL($url)) !== false) {
+    public function retrieve($url, $post = null) {
+        if(($content = $this->retrieveWithCURL($url, $post)) !== false) {
             return $content;
-        } elseif(($content = $this->retrieveWithFile($url)) !== false) {
+        } elseif(($content = $this->retrieveWithFile($url, $post)) !== false) {
             return $content;
         } else {
             return false;
         }
     }
 
-    public function retrieveWithCURL($url) {
+    public function retrieveWithCURL($url, $post) {
         if(!function_exists('curl_init')) {
             return false;
         }
         if(!($curl = @curl_init($url))) {
             return false;
         }
-        if(!@curl_setopt($curl, CURLOPT_RETURNTRANSFER, true)) {
+        $opts = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false
+        );
+        if ($post) {
+            $opts[CURLOPT_POST] = true;
+            $opts[CURLOPT_POSTFIELDS] = $post;
+        }
+        if (!@curl_setopt_array($curl, $opts)) {
             return false;
         }
         return @curl_exec($curl);
